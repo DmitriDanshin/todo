86383c92128de9b9d0b5506d6d587b1b85b0f0fc
Dmitri <Dmitri.Danshin2017@gmail.com>
2021-01-29 20:26:02 +0300

    Arrays

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const TodoListItem = () =>{
-    return <span>Drink Coffee</span>
+const TodoListItem = ({label, important = false, bold = false}) =>{
+    const liStyle = {
+        color: important ? 'tomato' : 'black',
+        fontWeight: bold ? 'bold' : '',
+    }
+    return <span style={liStyle}>{label}</span>
 }
 
 export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// 9
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,12 +8,16 @@ import TodoList from "./components/todo-list";
 import SearchPanel from "./components/search-panel";
 
 const App = () => {
-
+    const todoData = [
+        {label: "Drink Coffee", important: false, bold: true},
+        {label: "Build Awesome React App", important: true, bold: true},
+        {label: "Make something", important: false, bold: true},
+    ];
     return (
         <div>
             <AppHeader/>
             <SearchPanel/>
-            <TodoList/>
+            <TodoList todos = {todoData}/>
         </div>
     );
 }
